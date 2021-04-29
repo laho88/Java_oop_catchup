2acf6eb75b1391e57dedb676dd90fbb219a2800d
Levi <52985270+laho88@users.noreply.github.com>
2021-04-29 16:55:29 -0700

    SingleLinkedList Commit

---
+++ ListAssignment/SinglyLinkedList.java
@@ -0,0 +1,43 @@
+  
+public class SinglyLinkedList {
+    public Node head;
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if(head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while(runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }
+    public void remove(){
+        if(head != null){
+            Node runner = head;
+            while(runner.next.next != null){
+                runner = runner.next;
+            }
+            runner.next = null;
+        }
+    } 
+    public void printValues(){
+        if(head == null){
+            System.out.println("nothing to see");
+        }
+        else{
+            Node runner = head;
+            while (runner !=null){
+                System.out.println(runner.value);
+                runner = runner.next;
+            }
+        }
+
+    }
+    
+}
